b7fa6fc184af0956633c80fe036cbf05b13ce9e0
Jose Oliveros <oliverosjose@gmail.com>
2017-05-02 21:12:23 -0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+document.getElementById('myForm').addEventListener('submit', saveBookmark);
+
+function saveBookmark(e) {
+    var siteName = document.getElementById('siteName').value;
+    var siteUrl = document.getElementById('siteUrl').value;
+
+    var bookmark = {
+        name: siteName,
+        url: siteUrl
+    }
+
+    console.log(bookmark);
+    
+    e.preventDefault();
+}
